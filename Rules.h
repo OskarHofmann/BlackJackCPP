44ea96c7d59c4a954b7a22cff545daef26ef6746
Oskar Hofmann <oskar.hofmann@gmx.de>
2025-08-19 19:14:11 +0200

    Add rules for dealer

--- Rules.h
+++ Rules.h
@@ -4,7 +4,11 @@
 
 namespace BlackjackRules {
 
+	constexpr int bustScore{ 22 };
+	constexpr int dealerDrawLimit{ 17 };
+
 	int getCardValue(const Card& card);
 
 	int getHandScore(const std::vector<Card>& cards);
+
 }
